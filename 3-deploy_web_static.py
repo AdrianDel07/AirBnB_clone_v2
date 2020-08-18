8095c8e1bb7dcc9c226119e8687b75bf9fa70c08
AdrianDe07 <delaasuncionbuelvasadrian@gmail.com>
2020-08-17 22:32:01 -0500

    add Task 3 Full deployment

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,12 +1,33 @@
 #!/usr/bin/python3
-"""Write a Fabric script (based on the file 2-do_deploy_web_static.py) that \
-creates and distributes an archive to your web servers, \
-using the function deploy:"""
+"""Write a Fabric script (based on the file 1-pack_web_static.py) \
+that distributes an archive to your web servers, \
+using the function do_deploy"""
 
 from fabric.api import *
 from os import path
+from datetime import datetime
 
 env.hosts = ['34.75.68.113', '54.226.84.62']
+env.user = 'ubuntu'
+
+n = datetime.now()
+
+
+def do_pack():
+    """return the archive path if the archive has been correctly generated"""
+    file_name = 'versions/web_static_{}{}{}{}{}{}.tgz'.format(
+        n.year,
+        n.month,
+        n.day,
+        n.hour,
+        n.minute,
+        n.second)
+    local('mkdir -p versions')
+    command = local("tar -cvzf " + file_name + " ./web_static")
+    if command.succeeded:
+        return file_name
+    return None
+
 
 def do_deploy(archive_path):
     """Distributes an archive to your web servers,"""
@@ -18,28 +39,36 @@ def do_deploy(archive_path):
     if a.failed:
         ret_value = False
     _file = archive_path.replace(".tgz").replace("/versions/", "")
-    b = run('mkdir -p /data/web_static/releases/' + _file + '/')
+    b = run('mkdir -p /data/web_static/releases/' + archv + '/')
     if b.failed:
         ret_value = False
     c = run('tar -xzt tmp' + '.tgz' +
-            '-C /data/web_static/releases/' + _file + '/')
+            '-C /data/web_static/releases/' + archv + '/')
     if c.failed:
         ret_value = False
-    d = run('rm /tmp/' + _file + '.tgz')
+    d = run('rm /tmp/' + archv + '.tgz')
     if d.failed:
         ret_value = False
-    e = run('mv /data/web_static/releases' +_file +
-            '/web_static/* /data/web_static/releases/' + _file + '/web_static')
+    e = run('mv /data/web_static/releases' + archv +
+            '/web_static/* /data/web_static/releases/' + archv + '/web_static')
     if e.failed:
         ret_value = False
-    f = run('rm -r -f /data/web_static/releases' + _file + '/web_static')
+    f = run('rm -r -f /data/web_static/releases' + archv + '/web_static')
     if f.failed:
         ret_value = False
     g = run('rm -r -f /data/web_static/current')
     if g.failed:
         ret_value = False
-    h = run('ln -sf /data/web_static/releases' + _file + '/' +
+    h = run('ln -sf /data/web_static/releases' + archv + '/' +
             '/data/web_static/current')
     if h.failed:
         ret_value = False
     return (ret_value)
+
+
+def depploy():
+    """Distribute to all servers"""
+    archv_path = do_pack()
+    if archv_path is None:
+        return (False)
+    return (do_deploy(archv_path))
