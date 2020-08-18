3fcde4291dd00e6a000099eb69758dc1b122756c
AdrianDe07 <delaasuncionbuelvasadrian@gmail.com>
2020-08-17 20:52:19 -0500

    Add Task 1 Compress before sending

---
+++ versions/1-pack_web_static.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Write a Fabric script that generates a .tgz archive from the contents of \
+the web_static"""
+
+from fabric.api import run, local, sudo
+from datetime import datetime
+
+n = datetime.now()
+
+
+def do_pack():
+    """return the archive path if the archive has been correctly generated. """
+
+    file_name = 'versions/web_static_{}{}{}{}{}{}.tgz'\
+                .format(n.year, n.month, n.day, n.hour, n.minute, n.second)
+    local('mkdir -p versions')
+    command = local("tar -cvzf " + file_name + " ./web_static/")
+    if command.succeeded:
+        return file_name
+    return None
