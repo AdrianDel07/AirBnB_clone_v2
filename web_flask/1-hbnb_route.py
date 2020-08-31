b3cbd6b77173b1db791842abdde807559197fb44
AdrianDe07 <delaasuncionbuelvasadrian@gmail.com>
2020-08-31 13:14:22 -0500

    Add Task 1 HBNB fixing route

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -6,12 +6,12 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/hbnb', strict_slashes=False)
+@app.route('/', strict_slashes=False)
 def index():
     """display Hello HBNB!"""
     return ('Hello HBNB!')
 
-
+@app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """display HBNB"""
     return ('HBNB')
