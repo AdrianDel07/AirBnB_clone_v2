b882d29e73253ffa2eb7d5aebb724669cc834901
AdrianDe07 <delaasuncionbuelvasadrian@gmail.com>
2020-08-31 12:57:35 -0500

    Add Task 0 Hello Flask correctiing docstring

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -8,7 +8,8 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def index():
+    """display Hello HBNB!"""
     return ('Hello HBNB!')
 
 if __name__ == '__main__':
-    app.run(port=5000, host='0.0.0.0')
+    app.run(host='0.0.0.0', port=5000)
