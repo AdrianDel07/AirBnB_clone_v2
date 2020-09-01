5ec62e0186845a7be674229150dbd6d2c7e4c375
AdrianDe07 <delaasuncionbuelvasadrian@gmail.com>
2020-08-31 19:28:22 -0500

    Add Task 4 is it a number

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""start flask python <number>"""
+
+from flask import Flask
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def index():
+    """display Hello HBNB!"""
+    return ('Hello HBNB!')
+
+
+@app.route('/hbnb')
+def hbnb():
+    """display HBNB"""
+    return ('HBNB')
+
+
+@app.route('/c/<text>')
+def c_isfun(text):
+    """display C followed by value text"""
+    return ('C {}'.format(text.replace("_", " ")))
+
+
+@app.route('/python/')
+@app.route('/python/<text>')
+def python_text(text='is cool'):
+    """display Python followed by value text"""
+    return ('Python {}'.format(text.replace("_", " ")))
+
+
+@app.route('/number/<int:n>')
+def is_number(n):
+    """display n is a number"""
+    return ("{} is a number".format(n))
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
