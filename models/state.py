76da12a4332e7c17a573f94f02e6995eb772cdab
AdrianDe07 <delaasuncionbuelvasadrian@gmail.com>
2020-09-02 17:09:30 -0500

    Add Task 7 fixing errors

--- models/state.py
+++ models/state.py
@@ -22,8 +22,7 @@ class State(BaseModel, Base):
                               cascade="all, delete-orphan",
                               single_parent=True)
 
-    #if getenv("HBNB_TYPE_STORAGE") == "fs":
-    else:
+    if getenv("HBNB_TYPE_STORAGE") == "fs":
         @property
         def cities(self):
             """Return the list of City instances with state_id """
